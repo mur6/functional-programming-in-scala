646b715746951f7fbe7a4ffeee55f31ceab832cf
MURAKI Taichi <muraki.taichi@gmail.com>
2020-05-04 16:58:43 +0900

    no message

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,8 +1,14 @@
+import java.lang.Math
+
 object Main extends App {
   private def myAbs(n: Int): Int =
     if (n < 0) -n
     else n
 
-  val num = myAbs(-118)
-  println(s"Hello, World! my $num")
+  val num1 = myAbs(-118)
+  val num2 = Math.abs(-119)
+
+  val s1 = s"num1=$num1"
+  val s2 = s"num2=$num2"
+  println("Hello, World: " + s1 + ", " + s2)
 }
