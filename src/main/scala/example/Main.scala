96e8355ac0ed8ce06fd983d5c06a5e9bb6db8029
MURAKI Taichi <muraki.taichi@gmail.com>
2020-05-18 18:25:20 +0900

    Fix for another experiment.

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,14 +1,19 @@
 import java.lang.Math
 
+case class TwoNumbers(num1: Integer, num2: Integer) {
+  val absInteger1 = Math.abs(num1)
+  val absInteger2 = Math.abs(num2)
+
+  val s1 = s"abs1=$absInteger1"
+  val s2 = s"abs2=$absInteger2"
+}
+
 object Main extends App {
-  private def myAbs(n: Int): Int =
-    if (n < 0) -n
-    else n
-
-  val num1 = myAbs(-118)
-  val num2 = Math.abs(-119)
-
-  val s1 = s"num1=$num1"
-  val s2 = s"num2=$num2"
-  println("Hello, World: " + s1 + ", " + s2)
+//   private def myAbs(n: Int): Int =
+//     if (n < 0) -n
+//     else n
+  val nums = TwoNumbers(-100, -321)
+  println("Hello, World.")
+  println("TwoNumbers[1] = " + nums.s1)
+  println("TwoNumbers[2] = " + nums.s2)
 }
